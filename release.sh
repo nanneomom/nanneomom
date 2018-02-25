764b1539ad16646ace7d6832cba83331798c09d9
nanneomom <nanneomom@gmail.com>
2018-02-25 16:48:33 -0500

    add release.sh

---
+++ release.sh
@@ -0,0 +1,7 @@
+#keytool -genkey -v -keystore nanneo-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+ionic cordova build --release android &&\
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore nanneo-key.keystore /home/hokira/nanneomom/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name &&\
+zipalign -v 4 /home/hokira/nanneomom/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk nanneo.apk &&\
+apksigner verify nanneo.apk
+
+
