3cc0255e0d0dea0acacab9600e6d4c35fcd40b15
nanneomom <nanneomom@gmail.com>
2018-05-27 16:30:47 -0400

    improve chatroom

--- src/pages/chatroom/chatroom.ts
+++ src/pages/chatroom/chatroom.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import firebase from 'firebase';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Content, Events} from 'ionic-angular';
 
 import {ChatServiceProvider} from '../../providers/chat-service/chat-service';
 import {UserserviceProvider} from '../../providers/userservice/userservice'
@@ -13,12 +14,17 @@ import {UserserviceProvider} from '../../providers/userservice/userservice'
  */
 
     @IonicPage() @Component({selector: 'page-chatroom', templateUrl: 'chatroom.html',})
+
 export class ChatroomPage
 {
+  @ViewChild(Content) content: Content;
+  @ViewChild('chat_input') messageInput: ElementRef;
+
   editorMsg: string    = '';
   messages: Array<any> = [];
   db_ref               = null;
   otherUserId          = null;
+  otherUserName        = null;
 
   constructor(
       public navCtrl: NavController, public navParams: NavParams,
@@ -33,11 +39,38 @@ export class ChatroomPage
     this.otherUserId = this.navParams.get('otherUserId');
     console.log('talking with uid=' + this.otherUserId);
 
+    this.userServiceProvider.lookupUserData(
+        this.otherUserId, userData => {this.otherUserName = userData.firstName},
+        err => {
+          alert(err);
+        });
+
     this.db_ref = firebase.database().ref(
         'chatRooms/' + this.chatServiceProvider.getRoomId(this.otherUserId));
     this.db_ref.on('child_added', data => {
-      this.messages.push(data.val());
+      this.messages.push({
+        time: data.key,
+        name: data.val().name,
+        message: data.val().message,
+      });
     });
+  }
+
+
+  onFocus()
+  {
+    this.content.resize();
+    this.scrollToBottom();
+  }
+
+  scrollToBottom()
+  {
+    setTimeout(() => {
+      if (this.content.scrollToBottom)
+      {
+        this.content.scrollToBottom();
+      }
+    }, 400)
   }
 
   sendMsg()
